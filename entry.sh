23cda3253b3f04eb5ad29184a61fb4965140f6f6
Your Name <anuragpachauri8@gmail.com>
2024-09-12 14:01:27 +0530

    test 15

--- entry.sh
+++ entry.sh
@@ -6,10 +6,11 @@ CONFIG_FILE_1="app-config.yaml"
 # Choose the second config file based on the ENV environment variable
 if [ "$ENV" = "staging" ]; then
   CONFIG_FILE_2="app-config.staging.yaml"
-else
+elif [ "$ENV" = "production" ]; then
   CONFIG_FILE_2="app-config.production.yaml"
+else
+  CONFIG_FILE_2="app-config.yaml"
 fi
 
 # Run the backend with the two config files
 exec node packages/backend --config $CONFIG_FILE_1 --config $CONFIG_FILE_2
-
